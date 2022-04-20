c47a2f0e1ddc8e5f60001aa65289307838975940
John Payne <email@johnpayne.dev>
2022-04-19 23:25:01 -0400

    simple motor controlling

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -7,18 +7,76 @@
 #define JOYSTICK_RESOLUTION (1 << 12)
 #define RADIO_SYNC_BYTE     0x45
 
+typedef struct MotorPinout {
+	uint8_t en;
+	uint8_t in1;
+	uint8_t in2;
+} MotorPinout;
+
+const MotorPinout frontLeft  = { .en = 25, .in1 = 18, .in2 = 27 };
+const MotorPinout backLeft   = { .en = 26, .in1 = 16, .in2 = 17 };
+const MotorPinout frontRight = { .en = 33, .in1 = 21, .in2 = 19 };
+const MotorPinout backRight  = { .en = 32, .in1 = 23, .in2 = 22 };
+const MotorPinout motors[] = { frontLeft, backLeft, frontRight, backRight };
+
 BluetoothSerial SerialBT;
 
-struct Controller
-{
+struct Controller {
 	bool useWifi;
 	float xAxis;
 	float yAxis;
 	uint8_t button;
 } controller;
 
+void setup() {
+	Serial.begin(115200);
+	SerialBT.begin(BT_MOTOR_NAME, true);
+
+	for (uint8_t i = 0; i < 4; i++) {
+		pinMode(motors[i].en, OUTPUT);
+		pinMode(motors[i].in1, OUTPUT);
+		pinMode(motors[i].in2, OUTPUT);
+		analogWrite(motors[i].en, 255);
+		digitalWrite(motors[i].in1, LOW);
+		digitalWrite(motors[i].in2, HIGH);
+	}
+
+	controller = (struct Controller){ .useWifi = false };
+}
+
+void readRadioInput() {
+	while (SerialBT.available()) {
+		uint8_t syncByte = SerialBT.read();
+		if (syncByte != RADIO_SYNC_BYTE) { continue; }
+		uint8_t typeByte = SerialBT.read();
+		if (typeByte != 'x' && typeByte != 'y' && typeByte != 'b') { continue; }
+
+		uint8_t msb = SerialBT.read();
+		uint8_t lsb = SerialBT.read();
+		uint16_t input = (msb << 8) | lsb;
+		if (typeByte == 'x') { controller.xAxis = 2.0 * input / JOYSTICK_RESOLUTION - 1.0; }
+		if (typeByte == 'y') { controller.yAxis = 2.0 * input / JOYSTICK_RESOLUTION - 1.0; }
+		if (typeByte == 'b') { controller.button = input; }
+	}
+	Serial.printf("X:%f Y:%f B:%i\n", controller.xAxis, controller.yAxis, controller.button);
+}
+
+void loop() {
+	if (controller.useWifi) {
+		//
+	} else {
+		if (!SerialBT.connected()) {
+			Serial.printf("trying to connect to remote...\n");
+			if (!SerialBT.connect(BT_REMOTE_NAME)) { Serial.printf("failed to connect to remote, trying again...\n"); }
+		} else { readRadioInput(); }
+	}
+	
+	for (int8_t i = 0; i < 4; i++) { analogWrite(motors[i].en, controller.button ? 255 : 0); }
+	delay(10);
+}
+
 // only used for debugging purposes
-void searchDevices() {
+void searchBluetoothDevices() {
 	bool foundRemote = false;
 	while (!foundRemote) {
 		Serial.printf("Starting device search...\n");
@@ -41,33 +99,4 @@ void searchDevices() {
 			ESP.restart();
 		}
 	}
-}
-
-void setup() {
-	Serial.begin(115200);
-	SerialBT.begin(BT_MOTOR_NAME, true);
-
-	controller = (struct Controller){ .useWifi = false };
-	if (!SerialBT.connect(BT_REMOTE_NAME)) {
-		Serial.printf("failed to connect to remote, trying again...\n");
-		ESP.restart();
-	}
-	
-}
-
-void loop() {
-	while (SerialBT.available()) {
-		uint8_t syncByte = SerialBT.read();
-		if (syncByte != RADIO_SYNC_BYTE) { continue; }
-		uint8_t typeByte = SerialBT.read();
-		if (typeByte != 'x' && typeByte != 'y' && typeByte != 'b') { continue; }
-
-		uint8_t msb = SerialBT.read();
-		uint8_t lsb = SerialBT.read();
-		uint16_t value = (msb << 8) | lsb;
-		if (typeByte == 'x') { controller.xAxis = 2.0 * value / JOYSTICK_RESOLUTION - 1.0; }
-		if (typeByte == 'y') { controller.yAxis = 2.0 * value / JOYSTICK_RESOLUTION - 1.0; }
-		if (typeByte == 'b') { controller.button = value; }
-	}
-	Serial.printf("X:%f Y:%f B:%i\n", controller.xAxis, controller.yAxis, controller.button);
 }
